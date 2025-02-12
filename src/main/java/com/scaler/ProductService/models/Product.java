f4184ce75567d4969b4904374411e76e3d271668
ajayvagh <ajayvaghav8@gmail.com>
2025-02-12 12:26:41 +0530

    added Spring data JPA

--- src/main/java/com/scaler/ProductService/models/Product.java
+++ src/main/java/com/scaler/ProductService/models/Product.java
@@ -1,13 +1,18 @@
 package com.scaler.ProductService.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
 
+    @Id
+    private long id;
     String name;
     String category;
     String description;
